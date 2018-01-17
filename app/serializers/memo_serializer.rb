166a24f75c32035629317d7f874fdbdb7b2e6f28
melodyfs <melodyfs@users.noreply.github.com>
2018-01-17 00:00:49 -0800

    Push to heroku

--- app/serializers/memo_serializer.rb
+++ app/serializers/memo_serializer.rb
@@ -2,7 +2,7 @@ class MemoSerializer < ActiveModel::Serializer
   attributes :id, :title, :text_body, :date, :voice_file_url
 
   def voice_file_url
-      object.voice_file_url()
+      object.voice_file.url()
   end
 
 end
